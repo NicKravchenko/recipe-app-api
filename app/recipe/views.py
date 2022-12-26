1dbed149f701181b73a972994f14ac1f8df7e884
Nikita Kravchenko <89710564+NicKravchenko@users.noreply.github.com>
2022-12-26 13:16:15 -0400

    Added model of ingredients, create ingredient test works

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -13,6 +13,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import (
     Recipe,
     Tag,
+    Ingredient
 )
 
 from recipe import serializers
@@ -49,6 +50,22 @@ class TagViewSet(
     """Manage tags in the database"""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(
+        mixins.DestroyModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.ListModelMixin,
+        viewsets.GenericViewSet):
+    """Manage ingredients in the db"""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
